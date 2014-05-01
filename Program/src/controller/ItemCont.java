b4e7d4671638da9e253ef60dbd717ad9a3cc7d5d
søren <sren@10.111.180.4>
2014-05-01 10:13:04 +0200

    Bladr

--- Program/src/controller/ItemCont.java
+++ Program/src/controller/ItemCont.java
@@ -11,27 +11,28 @@ import com.mysql.jdbc.ResultSetMetaData;
 @SuppressWarnings("serial")
 public class ItemCont extends DefaultTableModel
 {
-	public ItemCont()
-	{
-		
-	}
-	// lav om til at parse til ArrayList.
+	private static ResultSetMetaData metaData;
+	private static int columnCount;
+	private static Vector<String> columnNames;
+	private static Vector<Vector<Object>> data;
+	private static Vector<Object> vector;
+	
 	public static DefaultTableModel buildTableModel(ResultSet rs)
 	        throws SQLException {
 
-	    ResultSetMetaData metaData = (ResultSetMetaData) rs.getMetaData();
+	    metaData = (ResultSetMetaData) rs.getMetaData();
 
 	    // names of columns
-	    Vector<String> columnNames = new Vector<String>();
-	    int columnCount = metaData.getColumnCount();
+	    columnNames = new Vector<String>();
+	    columnCount = metaData.getColumnCount();
 	    for (int column = 1; column <= columnCount; column++) {
 	        columnNames.add(metaData.getColumnName(column));
 	    }
 
 	    // data of the table
-	    Vector<Vector<Object>> data = new Vector<Vector<Object>>();
+	    data = new Vector<Vector<Object>>();
 	    while (rs.next()) {
-	        Vector<Object> vector = new Vector<Object>();
+	        vector = new Vector<Object>();
 	        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
 	            vector.add(rs.getObject(columnIndex));
 	        }
